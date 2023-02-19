1b82ed826f9f64a6553807c38287160efa87be75
ADITHYA KRISHNA <90098332+AdithyaKrishna169@users.noreply.github.com>
2023-02-19 12:10:28 +0530

    feat: add sitemap and robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://adithya-krishna.com/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), robotsTxt()]
   // vite: {
   //   ssr: {
   //     external: ["svgo"],
